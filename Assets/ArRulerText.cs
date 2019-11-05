aa57d3c18ffe790dc81d8c52584d8d77e4ea02cb
Rich <rsbeare@gmail.com>
2019-11-04 20:27:04 -0500

    End of session 10

---
+++ Assets/ArRulerText.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class ArRulerText : MonoBehaviour
+{
+    public TextMeshPro text; // Need a reference to the Text Mesh - need to make the text face the user at all times
+    public LineRenderer rulerLine; // Need reference to the ruler "line" - to ensure this is centered/positioned in the middle of the line
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
